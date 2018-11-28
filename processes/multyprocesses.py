d2388ea10a88935c1471bec0e427b972dd88d309
WizardZhT <956234606@qq.com>
2018-11-28 14:35:38 +0800

    Update multyprocesses.py

--- processes/multyprocesses.py
+++ processes/multyprocesses.py
@@ -2,10 +2,13 @@
 import os
 import time
 while True:
-	time.sleep(5)
+	time.sleep(3)
 	print('Process (%s) start...'% os.getpid())
+	time.sleep(3)
 	pid = os.fork()
 	if pid==0:
+		time.sleep(3)
 	    print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
 	else:
+		time.sleep(3)
 	    print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
