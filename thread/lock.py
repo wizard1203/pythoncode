6daea62d6317b0d0d050a2154ad8285647944e54
WizardZhT <956234606@qq.com>
2018-11-28 15:35:22 +0800

    Create lock.py

---
+++ thread/lock.py
@@ -0,0 +1,21 @@
+import time, threading
+#saving in bank
+balance = 0
+
+def change_it(n):
+    global balance
+    # save money before get it 
+    balance = balance + n
+    balance = balance - n
+
+def run_thread(n):
+    for i in range(100000):
+        change_it(n)
+
+t1 = threading.Thread(target=run_thread, args=(5,))
+t2 = threading.Thread(target=run_thread, args=(8,))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+print balance
