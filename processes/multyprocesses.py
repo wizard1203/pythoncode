e4c0af57d54561b6fa306fbbde9d410fab6553a1
WizardZhT <956234606@qq.com>
2018-11-28 14:17:34 +0800

    Update multyprocesses.py

--- processes/multyprocesses.py
+++ processes/multyprocesses.py
@@ -1,9 +1,11 @@
 # multiprocessing.py
 import os
-
-print('Process (%s) start...'% os.getpid())
-pid = os.fork()
-if pid==0:
-    print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
-else:
-    print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+import time
+while True:
+	time.sleep(5)
+	print('Process (%s) start...'% os.getpid())
+	pid = os.fork()
+	if pid==0:
+	    print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+	else:
+	    print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
